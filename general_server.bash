9c7f12aff32da33effffaceb0b8b9014d74da79b
jacobitosuperstar <jacobomateobedoya@iclud.com>
2020-05-06 20:52:59 +0000

    preparador del servidor

--- general_server.bash
+++ general_server.bash
@@ -1,5 +1,9 @@
-echo "hola mundo"
+echo "iniciando el servidor"
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
+sudo apt-get -y git
+sudo apt-get -y vim
 sudo apt-get install pipenv -y
+
 echo "preparado el servidor"
